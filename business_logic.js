e9eaefb5c1ba4d1426f67b820eeae73e092fa264
Anmol <anmolpokhrel46@gmail.com>
2021-07-23 18:37:39 +0530

    updated  complete branch

--- business_logic.js
+++ business_logic.js
@@ -1,53 +1,57 @@
-function parseQuestion (rawQuestion) {
-const formattedQuestions = [];
-for (let question of rawQuestion) {
-const formattedQuestion = {
-    question: question.question,
-    Option: [question.correct_answer, ...question.incorrect_answers],
-    correct_answer: question.correct_answer,
-     
-};
-// typo error was here
+function parseQuestions(rawQuestions) {
+  const formattedQuestions = [];
+  for (let question of rawQuestions) {
+    const formattedQuestion = {
+      question: question.question,
+      options: getRandomizedArray ([question.correct_answer, ...question.incorrect_answers]),
+      correct_answer: question.correct_answer,
+    };
     formattedQuestions.push(formattedQuestion);
+  }
+  return formattedQuestions;  
 }
+
+const getQuestions = async (count) => {
+  try {
+    const response = await fetch(
+      `https://opentdb.com/api.php?amount=${count}&category=18&difficulty=medium&type=multiple`
+    );
+    const data = await response.json();
+    const questions = await data.results;
+    const formattedQuestions = parseQuestions(questions);
+    if (questions.length === 0) {
+      console.log("Please check the url");
+    }
     return formattedQuestions;
-
-}
-
-const getQuestion = async (count) => {
-    try {
-        const response = await fetch(
-            `https://opentdb.com/api.php?amount=${count}&category=18&difficulty=medium&type=multiple`
-
-        );
-        const data = await response.json();
-        const question = await data.results;
-        const formattedQuestion = parseQuestion(question);
-        if (question.length === 0) {
-            console.log("please check the url");
-        }
-        return formattedQuestion;
-    }catch (error) {
-        console.log(error);
-    }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-getQuestion(3);
-const getCorrectAnswer = (question) => {
-    let answer = [];
-    for (let q of question) {
-        answer.push(q.correct_answer);
-    }
-    return answers;
+const getCorrectAnswers = (questions) => {
+  let answers = [];
+  for (let q of questions) {
+    answers.push(q.correct_answer);
+  }
+  return answers;
+};
+ 
+const getOptions = (questions) => {
+  let options = [];
+  for (let q of questions) {
+    options.push(q.options);
+  }
+  return options;
 };
 
-const getOptions = (question) => {
-    let getOptions = [];
-    for (let q of question) {
-        answer.push(q.options);
+const calculateScore = (correctAns, userAnswers) => {
+  let correctCount = 0;
+  // Assuming ans and question array have same length
+  for (let i = 0; i < userAnswers.length; i++) {
+    if (userAnswers[i] === correctAns[i]) {
+      correctCount++;
     }
-    return options;
+  }
+  return correctCount;
+ 
 };
-    
-
-
--- main.js
+++ main.js
@@ -1,51 +1,150 @@
-const root = document.getElementById("root");
-console.log(root);
-let playButton = document.createElement("Button");
-let nextButton = document.createElement("Button");
-let heading = document.createElement("h1");
-let question = document.createElement("h3");
-const inputField = document.createElement("input");
-// Loading screen
-const loadingscreen = document.createElement("h1");
-loadingscreen.innerText = "Loading....";
-// state variable
+const root = document.getElementById('root');
+let playButton = document.createElement('button');
+let nextButton = document.createElement('button');
+let heading = document.createElement('h1');
+let question = document.createElement('h3');
+const inputField = document.createElement('input');
+
+// Loading Screeen
+const loadingScreen = document.createElement('h1');
+loadingScreen.innerText = 'Loading....';
+// STATE VARIABLES
 let questionCount;
 let questions;
 let questionId = 0;
-
+let getOnlyQsn;
+let allOption;
+let options;
+let optionDiv;
+let selectedAns = null;
+let allSelectedAns = [];
 const paintLandingPage = () => {
-  inputField.type = "number";
-  inputField.min = 3;
-  inputField.max = 10;
-  playButton.innerText = "Play";
-  heading.innerText = "PLAY QUIZ";
-  root.appendChild(heading);
-  root.appendChild(inputField);
-  root.appendChild(playButton);
+
+	inputField.type = 'number';
+	inputField.min = 3;
+	inputField.max = 10;
+	playButton.innerText = 'Play';
+
+	heading.innerText = 'Play Quiz';
+	root.appendChild(heading);
+	root.appendChild(inputField);
+	root.appendChild(playButton);
 };
 paintLandingPage();
 
-
-const paintQuestion = (questionId, buttonText) => {
-  root.innerText = null;
-  heading.innerText = "Question";
-  question.innerText = questionId + 1;
-  nextButton.innerText = buttonText;
-  root.appendChild(heading);
-  root.appendChild(question);
-  root.appendChild(nextButton);
+let getOnlyQuestion = (questions) => {
+	return `Q  ${questionId + 1}. ${questions[questionId].question} `;
 };
 
-playButton.addEventListener ("click", async () => {
-questionCount = inputField.value;
-if (questionCount >= 3) {
-  root.innerHTML = null;
-  root.appendChild (loadingscreen);
-  questions = await getQuestion(questionCount);
-  root.innerHTML = null;
-  paintQuestion (questionId , "next");
-}
-return;
+const paintQuestion = (questionId, buttonText) => {
+	root.innerHTML = null;
+	heading.innerText = 'questions';
+	question.innerText = getOnlyQuestion(questions);
+	nextButton.innerText = buttonText;
+	root.appendChild(heading);
+	root.appendChild(question);
+	root.appendChild(nextButton);
+};
+
+const showOptionsHtml = () => {
+	options = getOptions(questions)[questionId];
+	let optionsFragements = new DocumentFragment();
+	options.map((option) => {
+		optionDiv = document.createElement('div');
+		let radioButtons = document.createElement('input');
+		radioButtons.type = 'radio';
+		radioButtons.className = "radio-btn"
+		radioButtons.value = option;
+		radioButtons.name = 'radio';
+		const labelForRadioButton = document.createElement('label');
+		labelForRadioButton.htmlFor = option;
+		labelForRadioButton.innerText = option;
+		optionDiv.appendChild(radioButtons);
+		optionDiv.appendChild(labelForRadioButton);
+		optionsFragements.appendChild(optionDiv);
+	});
+	return optionsFragements;
+};
+
+playButton.addEventListener('click', async () => {
+	// Check if input value is more than 3
+	questionCount = inputField.value;
+	if (questionCount >= 3) {
+		// Fetch question
+		// Paint screen with first question
+		// Show loading screen
+		root.innerHTML = null;
+		root.appendChild(loadingScreen);
+		questions = await getQuestions(questionCount);
+		root.innerHTML = null;
+		paintQuestion(questionId, 'Next');
+		allOption = showOptionsHtml();
+
+		root.appendChild(allOption);
+	}
+
+	return;
 });
 
 
+
+//  checking and pusing values in the ans array
+getCheckedRadioButton = () => {
+const allRadioButton = document.querySelectorAll('.radio-btn')	
+	allRadioButton.forEach((elem) => {
+		if (elem.checked) {
+			selectedAns = elem.value
+		}else if (selectedAns === null) {
+			selectedAns = " ";
+		}
+	
+	})
+	allSelectedAns.push(selectedAns);
+	selectedAns = null;
+
+
+}
+
+	
+// NEXT BUTTON LOGIC
+
+nextButton.addEventListener('click', () => {
+	//TODO:  Save Answer
+	let nextButtonText = questionId < questions.length - 2 ? 'Next' : 'Submit';
+	if (questionId < questions.length -1 ) {
+		// Save Answer here
+
+		getCheckedRadioButton()
+		questionId++;
+		paintQuestion(questionId, nextButtonText);
+     	allOption = showOptionsHtml();
+
+		root.appendChild(allOption);
+
+	} else {
+		// calculateFinalScore(questions)
+	calculateFinalScore()	
+		getCheckedRadioButton()
+		showResultPage();
+	}
+});
+
+// calculate correct ans 
+const calculateFinalScore = () => {
+	
+    let correctAns = getCorrectAnswers(questions)
+	let score = calculateScore(correctAns, allSelectedAns);
+	return score
+}
+
+
+const showResultPage = () => {
+	// TODO: Call get score function and  change score inner text
+	// TODO: Add replay button so that the dom paints landing page screen
+	root.innerHTML = null;
+	heading.innerText = ' Your Score Is ';
+	const score = document.createElement('h1');
+	score.innerText = `${calculateFinalScore()}/${  questionCount }`;
+	root.appendChild(heading);
+	root.appendChild(score);
+};
